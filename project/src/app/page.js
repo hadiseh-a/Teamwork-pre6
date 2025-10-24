52e2978917399728118fbb50517f81ffbdeb96ec
samane ghodrati <samane.ghodrati@gmail.com>
2025-10-23 21:40:39 -0900

    add

--- project/src/app/page.js
+++ project/src/app/page.js
@@ -1,12 +1,10 @@
-
 import styles from "./page.module.css";
-import QuestionPage from "./component/QuestionPage";
+import QuestionPage from "./component/HomePage";
 
 export default function Home() {
   return (
-    <div >
-              <QuestionPage/>
-  
+    <div>
+      <QuestionPage />
     </div>
   );
 }
