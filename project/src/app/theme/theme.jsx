bb2241d459d8508419d6840262142a466de3a66e
helia lorestani <lorestanihelia0@gmail.com>
2025-10-24 12:21:19 +0330

    changes on theme

--- project/src/app/theme/theme.jsx
+++ project/src/app/theme/theme.jsx
@@ -1,13 +1,14 @@
 import { createTheme } from "@mui/material/styles";
 
-export const getTheme = (mode) =>createTheme({
+export const getTheme = (mode) =>
+  createTheme({
     palette: {
-        mode,
-        primary:{
-            main: mode === 'light'?'#1976d2' : '#90caf9'
-        },
-        background : {
-            default: mode=== 'light'? '#fff' : '#121212'
-        }
-    }
-})
+      mode,
+      primary: {
+        main: mode === "light" ? "#87469fff" : "#280a33ff",
+      },
+      background: {
+        default: mode === "light" ? "#fff" : "#101010ff",
+      },
+    },
+  });
