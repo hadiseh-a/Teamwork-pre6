76fb89e3a2f302daf791e55d7a62c40a638208d5
rasa0081 <rasa001381@gmail.com>
2025-10-24 19:32:35 +0330

    debugging some errors

--- project/src/app/About/page.jsx
+++ project/src/app/About/page.jsx
@@ -4,29 +4,39 @@ import { Instagram as InstagramIcon, Facebook as FacebookIcon, Twitter as Twitte
 
 export default function AboutPage(){
     return (
-        <Box sx={{p:4}}>
-            <Typography variant="h4" gutterBottom>
-                About the Web
-            </Typography>
-            <Typography variant="body1" paragraph>
-                tesssssssssssssssssssssssssssst
-            </Typography>
-            <Typography variant="h6" gutterBottom>
-                connect with us
-            </Typography>
-            <Stack direction='row' spacing={2}>
-                <Link href='https://instagram.com/sample' target='blank' rel='noopener' color="inherit">
-                <InstagramIcon fontSize="large"/>
-                </Link>
-                <Link href='https://facebook.com/sample' target='blank' rel='noopener' color="inherit">
-                <FacebookIcon fontSize="large"/>
-                </Link>
-                <Link href='https://x.com/sample' target='blank' rel='noopener' color="inherit">
-                <TwitterIcon fontSize="large"/>
-                </Link>
+        <Box 
+            sx={{
+                minHeight:'100vh',
+                display:'flex',
+                flexDirection:'column',
+                justifyContent:'space-between',
+                p:4
+            }}> 
+            <Box display={'flex'}  flexDirection={'column'} justifyContent={'center'}>
+                <Typography variant="h4" gutterBottom>
+                    About the Web
+                </Typography>
+                <Typography variant="body1" paragraph>
+                    tesssssssssssssssssssssssssssst
+                </Typography>
+            </Box>
+            <Box sx={{textAlign:'center', mt:4}}>
+                <Typography variant="h6" gutterBottom>
+                    connect with us
+                </Typography>
+                <Stack direction='row' display={'flex'} justifyContent={'center'} spacing={2}>
+                    <Link href='https://instagram.com/sample' target='blank' rel='noopener' color="inherit">
+                    <InstagramIcon fontSize="large"/>
+                    </Link>
+                    <Link href='https://facebook.com/sample' target='blank' rel='noopener' color="inherit">
+                    <FacebookIcon fontSize="large"/>
+                    </Link>
+                    <Link href='https://x.com/sample' target='blank' rel='noopener' color="inherit">
+                    <TwitterIcon fontSize="large"/>
+                    </Link>
 
-            </Stack>
-
+                </Stack>
+            </Box>
         </Box>
     )
 }
